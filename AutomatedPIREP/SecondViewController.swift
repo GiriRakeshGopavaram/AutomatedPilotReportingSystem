4c2a125ee848e7549f4ff7acf4c877bd6b3405f3
Student <s525882@ol23203m.nwmissouri.edu>
2016-10-19 21:13:49 -0500

    Initial Commit

---
+++ AutomatedPIREP/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AutomatedPIREP
+//
+//  Created by Gopavaram,Giri Rakesh on 10/19/16.
+//  Copyright © 2016 Gopavaram,Giri Rakesh. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

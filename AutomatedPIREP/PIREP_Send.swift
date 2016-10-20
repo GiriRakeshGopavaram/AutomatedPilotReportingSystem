665b47ef5eb3dbf9eb93d26ab5d2802c714d4a6b
Student <s525882@ol23203m.nwmissouri.edu>
2016-10-19 21:18:54 -0500

    Plot locations

---
+++ AutomatedPIREP/PIREP_Send.swift
@@ -0,0 +1,58 @@
+//
+//  SecondViewController.swift
+//  AutomatedPIREP
+//
+//  Created by Gopavaram,Giri Rakesh on 10/13/16.
+//  Copyright © 2016 Gopavaram,Giri Rakesh. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class PIREP_Send: UIViewController,MKMapViewDelegate,CLLocationManagerDelegate {
+
+    @IBOutlet weak var pirepView: MKMapView!
+    let locationManager  = CLLocationManager()
+    let airportLocation = LocationFactory.storeLocations()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+        self.pirepView.showsUserLocation = true
+        
+        for airport in airportLocation{
+            let latitude = airport.latitude
+            let longitude = airport.longitude
+            let location = CLLocationCoordinate2DMake(latitude, longitude)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = location
+            pirepView.addAnnotation(annotation)
+        }
+    }
+  
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        let center = CLLocationCoordinate2DMake(location!.coordinate.latitude, location!.coordinate.longitude)
+        let region = MKCoordinateRegionMake(center, MKCoordinateSpan(latitudeDelta: 95,longitudeDelta: 95))
+        self.pirepView.setRegion(region, animated: true)
+        self.locationManager.stopUpdatingLocation()
+    }
+
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("Errors" + error.localizedDescription)
+    }
+    
+    
+}
+
---
+++ AutomatedPIREP/locations.swift
@@ -0,0 +1,18 @@
+//
+//  locations.swift
+//  test1
+//
+//  Created by Gopavaram,Giri Rakesh on 10/13/16.
+//  Copyright © 2016 Gopavaram,Giri Rakesh. All rights reserved.
+//
+
+import Foundation
+class locations{
+    var latitude:Double
+    var longitude:Double
+    init(latitude:Double, longitude:Double){
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+    
+}

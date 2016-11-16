830606f0affe302a45827d7fff36fb6fc3957fa7
Student <s525882@ch140003m.nwmissouri.edu>
2016-11-16 17:43:51 -0600

    Winds at 30,000 feet are being displayed

--- AutomatedPIREP/METARSViewController.swift
+++ AutomatedPIREP/METARSViewController.swift
@@ -13,7 +13,6 @@ import CoreLocation
 class METARSViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelegate {
 
 
-    @IBOutlet weak var metOrbarb: MKMapView!
     //var jsonURL:String = "MetarJSON"
     
     @IBAction func metOrBarb(sender: AnyObject) {
@@ -36,36 +35,33 @@ class METARSViewController: UIViewController,MKMapViewDelegate,CLLocationManager
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        super.viewDidLoad()
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestAlwaysAuthorization()
-        locationManager.startUpdatingLocation()
+        //locationManager.startUpdatingLocation()
         pirepView.delegate = self
  
         
     }
 
     func makeMetarRequest(){
-        let url = NSURL(string: "https://new.aviationweather.gov/gis/scripts/MetarJSON.php")
+        let url = NSURL(string: "http:/aviationweather.gov/gis/scripts/MetarJSON.php")
         
         // send out the request
         let session = NSURLSession.sharedSession()
         // implement completion handler
-        session.dataTaskWithURL(url!, completionHandler: processResults1).resume()
-        
+        session.dataTaskWithURL(url!, completionHandler: processMETARS).resume()
     }
     func makeWindRequest(){
-        let url = NSURL(string: "https://new.aviationweather.gov/gis/scripts/FBWindsJSON.php")
+        let url = NSURL(string: "http:/aviationweather.gov/gis/scripts/FBWindsJSON.php")
         
         // send out the request
         let session = NSURLSession.sharedSession()
         // implement completion handler
-        session.dataTaskWithURL(url!, completionHandler: processResults2).resume()
-        
+        session.dataTaskWithURL(url!, completionHandler: processWinds).resume()
     }
     
-    func processResults1(data:NSData?,response:NSURLResponse?,error:NSError?)->Void {
+    func processMETARS(data:NSData?,response:NSURLResponse?,error:NSError?)->Void {
         do {
             
             // parse the data as dictionary first
@@ -111,7 +107,7 @@ class METARSViewController: UIViewController,MKMapViewDelegate,CLLocationManager
         }
     }
     
-    func processResults2(data:NSData?,response:NSURLResponse?,error:NSError?)->Void {
+    func processWinds(data:NSData?,response:NSURLResponse?,error:NSError?)->Void {
         do {
             
             // parse the data as dictionary first
@@ -126,24 +122,114 @@ class METARSViewController: UIViewController,MKMapViewDelegate,CLLocationManager
                     
                     dispatch_async(dispatch_get_main_queue(), {
                         for eachObject in results{
-                            
                             let geometry:[String:AnyObject] = eachObject["geometry"] as! [String:AnyObject]
-                            //let properties:[String:AnyObject] = eachObject["properties"] as! [String:AnyObject]
                             var coordinates:[Double] = []
                             var latitude:Double
                             var longitude:Double
+                            
                             coordinates = geometry["coordinates"] as! [Double]
                             longitude = (coordinates[0])
                             latitude = (coordinates[1])
+                            
                             let location = CLLocationCoordinate2DMake(latitude, longitude)
                             let annotation = CustomPointAnnotation()
                             annotation.coordinate = location
-                            annotation.pinCustomImageName = UIImage(named: "Nil.png")
+                            
+                            
+                            let properties:[String:AnyObject] = eachObject["properties"] as! [String:AnyObject]
+                            let windSpeedAsString:String = properties["wspd"] as! String
+                            let windSpeed:Int? = Int(windSpeedAsString)
+                            
+                            let windDirectionAsString = properties["wdir"] as! String
+                            let windDirection:Int? = Int(windDirectionAsString)
+                            
+                            let stationId = properties["id"] as! String
+                            
+                            if windSpeedAsString.containsString("nil"){
+                                print("Sorry, I don't want a nil")
+                            }
+                            else{
+                                
+//                                for speed in 0 ... 18{
+//                                    
+//                                }
+                                if Int(windSpeed!) < 1 {
+                                    let windDirectionImage:UIImage = UIImage(named: "Calm")!
+                                    annotation.pinCustomImageName = windDirectionImage.imageRotatedByDegrees(CGFloat(windDirection!)+90, flip: false)
+                                }
+                                else if Int(windSpeed!) > 0 && Int(windSpeed!) < 3{
+                                    let windDirectionImage:UIImage = UIImage(named: "1-2")!
+                                    annotation.pinCustomImageName = windDirectionImage.imageRotatedByDegrees(CGFloat(windDirection!)+90, flip: false)
+                                }
+                                else if Int(windSpeed!) > 2 && Int(windSpeed!) < 8{
+                                    let windDirectionImage:UIImage = UIImage(named: "3-7")!
+                                    annotation.pinCustomImageName = windDirectionImage.imageRotatedByDegrees(CGFloat(windDirection!)+90, flip: false)
+                                }
+                                else if Int(windSpeed!) > 7 && Int(windSpeed!) < 13{
+                                    let windDirectionImage:UIImage = UIImage(named: "8-12")!
+                                    annotation.pinCustomImageName = windDirectionImage.imageRotatedByDegrees(CGFloat(windDirection!)+90, flip: false)
+                                }
+                                else if Int(windSpeed!) > 12 && Int(windSpeed!) < 18{
+                                    let windDirectionImage:UIImage = UIImage(named: "13-17")!
+                                    annotation.pinCustomImageName = windDirectionImage.imageRotatedByDegrees(CGFloat(windDirection!)+90, flip: false)
+                                }
+                                else if Int(windSpeed!) > 17 && Int(windSpeed!) < 23{
+                                    let windDirectionImage:UIImage = UIImage(named: "18-22")!
+                                    annotation.pinCustomImageName = windDirectionImage.imageRotatedByDegrees(CGFloat(windDirection!)+90, flip: false)
+                                }
+                                else if Int(windSpeed!) > 22 && Int(windSpeed!) < 28{
+                                    let windDirectionImage:UIImage = UIImage(named: "23-27")!
+                                    annotation.pinCustomImageName = windDirectionImage.imageRotatedByDegrees(CGFloat(windDirection!)+90, flip: false)
+                                    
+                                }
+                                else if Int(windSpeed!) > 27 && Int(windSpeed!) < 33{
+                                    let windDirectionImage:UIImage = UIImage(named: "28-32")!
+                                    annotation.pinCustomImageName = windDirectionImage.imageRotatedByDegrees(CGFloat(windDirection!)+90, flip: false)
+                                    
+                                }
+                                else if Int(windSpeed!) > 32 && Int(windSpeed!) < 38{
+                                    let windDirectionImage:UIImage = UIImage(named: "33-37")!
+                                    annotation.pinCustomImageName = windDirectionImage.imageRotatedByDegrees(CGFloat(windDirection!)+90, flip: false)
+                                    
+                                }
+                                else if Int(windSpeed!) > 37 && Int(windSpeed!) < 43{
+                                    let windDirectionImage:UIImage = UIImage(named: "38-42")!
+                                    annotation.pinCustomImageName = windDirectionImage.imageRotatedByDegrees(CGFloat(windDirection!)+90, flip: false)
+                                    
+                                }
+                                else if Int(windSpeed!) > 42 && Int(windSpeed!) < 48{
+                                    let windDirectionImage:UIImage = UIImage(named: "43-47")!
+                                    annotation.pinCustomImageName = windDirectionImage.imageRotatedByDegrees(CGFloat(windDirection!)+90, flip: false)
+                                }
+                                else if Int(windSpeed!) > 47 && Int(windSpeed!) < 53{
+                                    let windDirectionImage:UIImage = UIImage(named: "48-52")!
+                                    annotation.pinCustomImageName = windDirectionImage.imageRotatedByDegrees(CGFloat(windDirection!)+90, flip: false)
+                                }
+                                else if Int(windSpeed!) > 52 && Int(windSpeed!) < 58{
+                                    let windDirectionImage:UIImage = UIImage(named: "53-57")!
+                                    annotation.pinCustomImageName = windDirectionImage.imageRotatedByDegrees(CGFloat(windDirection!)+90, flip: false)
+                                }
+                                else if Int(windSpeed!) > 57 && Int(windSpeed!) < 63{
+                                    let windDirectionImage:UIImage = UIImage(named: "58-62")!
+                                    annotation.pinCustomImageName = windDirectionImage.imageRotatedByDegrees(CGFloat(windDirection!)+90, flip: false)
+                                }
+                                else if Int(windSpeed!) > 62 && Int(windSpeed!) < 68{
+                                    let windDirectionImage:UIImage = UIImage(named: "63-67")!
+                                    annotation.pinCustomImageName = windDirectionImage.imageRotatedByDegrees(CGFloat(windDirection!)+90, flip: false)
+                                }
+                                else if Int(windSpeed!) > 67 && Int(windSpeed!) < 73{
+                                    let windDirectionImage:UIImage = UIImage(named: "68-72")!
+                                    annotation.pinCustomImageName = windDirectionImage.imageRotatedByDegrees(CGFloat(windDirection!)+90, flip: false)
+                                }
+                                
+                            }
+                            annotation.title = "FB Winds over \(stationId)"
+                            annotation.subtitle = "Winds:\(windDirection!) at \(windSpeed!) kt"
+                            //annotation.subtitle = "WindSpeed \(windSpeed!)"
                             self.pirepView.addAnnotation(annotation)
                             
                         }
                     })
-                    
                 }
             }
                 
@@ -185,4 +271,48 @@ class METARSViewController: UIViewController,MKMapViewDelegate,CLLocationManager
     
     
 }
+extension UIImage {
+    public func imageRotatedByDegrees(degrees: CGFloat, flip: Bool) -> UIImage {
+        let radiansToDegrees: (CGFloat) -> CGFloat = {
+            return $0 * (180.0 / CGFloat(M_PI))
+        }
+        let degreesToRadians: (CGFloat) -> CGFloat = {
+            return $0 / 180.0 * CGFloat(M_PI)
+        }
+        
+        // calculate the size of the rotated view's containing box for our drawing space
+        let rotatedViewBox = UIView(frame: CGRect(origin: CGPointZero, size: size))
+        let t = CGAffineTransformMakeRotation(degreesToRadians(degrees));
+        rotatedViewBox.transform = t
+        let rotatedSize = rotatedViewBox.frame.size
+        
+        // Create the bitmap context
+        UIGraphicsBeginImageContext(rotatedSize)
+        let bitmap = UIGraphicsGetCurrentContext()
+        
+        // Move the origin to the middle of the image so we will rotate and scale around the center.
+        CGContextTranslateCTM(bitmap, rotatedSize.width / 2.0, rotatedSize.height / 2.0);
+        
+        //   // Rotate the image context
+        CGContextRotateCTM(bitmap, degreesToRadians(degrees));
+        
+        // Now, draw the rotated/scaled image into the context
+        var yFlip: CGFloat
+        
+        if(flip){
+            yFlip = CGFloat(-1.0)
+        } else {
+            yFlip = CGFloat(1.0)
+        }
+        
+        CGContextScaleCTM(bitmap, yFlip, -1.0)
+        CGContextDrawImage(bitmap, CGRectMake(-size.width / 2, -size.height / 2, size.width, size.height), CGImage)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+}
+
 

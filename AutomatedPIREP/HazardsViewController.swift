6d6718ed1ee6a4fd8320bd1a3dedfb7d68d918a9
admin <admin@admins-macbook-pro.local>
2016-11-27 21:11:46 -0600

    Added code to fetch data from hazards JSON

--- AutomatedPIREP/HazardsViewController.swift
+++ AutomatedPIREP/HazardsViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 import MapKit
 
 class HazardsViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelegate {
-
+    
     
     @IBOutlet weak var mapView: MKMapView!
     let locationManager = CLLocationManager()
@@ -29,9 +29,72 @@ class HazardsViewController: UIViewController,MKMapViewDelegate,CLLocationManage
         mapView.delegate = self
         mapView.mapType = MKMapType.Standard
         mapView.showsUserLocation = true
+        makeRequest()
+    }
+    
+    func makeRequest(){
+        let url = NSURL(string: "http://aviationweather.gov/gis/scripts/HazardJSON.php")
+        
+        // send out the request
+        let session = NSURLSession.sharedSession()
+        // implement completion handler
+        session.dataTaskWithURL(url!, completionHandler: processResults).resume()
         
     }
     
+    func processResults(data:NSData?,response:NSURLResponse?,error:NSError?)->Void {
+        do {
+            
+            // parse the data as dictionary first
+            var jsonResult: NSDictionary?
+            try jsonResult =  NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as? NSDictionary
+            
+            // now if jsonResult actually contains something
+            if (jsonResult != nil) {
+               
+                if let results = jsonResult!["features"] as? [NSDictionary] {
+                
+                    dispatch_async(dispatch_get_main_queue(), {
+                    
+                        for eachObject in results {
+                            
+                            //print(eachObject["geometry"] as! String)
+                            
+                            if let geometry = eachObject["geometry"] as? NSDictionary{
+                                if geometry["type"] as! String == "Point"{
+                                let lineCoordinates = geometry["coordinates"]
+                                print(lineCoordinates)
+                                } else if geometry["type"] as! String == "Polygon" {
+                                
+                                    let polygonCoordinates = geometry["coordinates"]
+                                    print(polygonCoordinates)
+                                
+                                } else if geometry["type"] as! String == "MultiPolygon"{
+                                
+                                    let multiPolygonCoordinates = geometry["coordinates"]
+                                    print(multiPolygonCoordinates)
+                                } else {
+                                
+                                 print(geometry["type"] as! String)
+                                }
+                            }
+                            
+                            
+                            
+                           
+                        
+                        }
+                        
+                    })
+                
+                }
+            }
+        }
+        catch {
+            
+        }
+        
+    }
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         

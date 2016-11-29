a08689be0c0ef89fbef34bba67c3243ce8fa0c6a
admin <admin@admins-macbook-pro.local>
2016-11-29 01:08:35 -0600

    Added code for overlays in Hazards controller

--- AutomatedPIREP/HazardsViewController.swift
+++ AutomatedPIREP/HazardsViewController.swift
@@ -10,6 +10,7 @@
 import UIKit
 import MapKit
 
+
 class HazardsViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelegate {
     
     
@@ -29,6 +30,7 @@ class HazardsViewController: UIViewController,MKMapViewDelegate,CLLocationManage
         mapView.delegate = self
         mapView.mapType = MKMapType.Standard
         mapView.showsUserLocation = true
+        drawShape()
         makeRequest()
     }
     
@@ -51,42 +53,48 @@ class HazardsViewController: UIViewController,MKMapViewDelegate,CLLocationManage
             
             // now if jsonResult actually contains something
             if (jsonResult != nil) {
-               
-                if let results = jsonResult!["features"] as? [NSDictionary] {
                 
-                    dispatch_async(dispatch_get_main_queue(), {
+                if let results = jsonResult!["features"] as? [NSDictionary] {
                     
+                    dispatch_async(dispatch_get_main_queue(), {
+                        
                         for eachObject in results {
                             
                             //print(eachObject["geometry"] as! String)
                             
                             if let geometry = eachObject["geometry"] as? NSDictionary{
                                 if geometry["type"] as! String == "Point"{
-                                let lineCoordinates = geometry["coordinates"]
-                                print(lineCoordinates)
+                                    let pointCoordinates = geometry["coordinates"]
+                                    print(pointCoordinates)
                                 } else if geometry["type"] as! String == "Polygon" {
-                                
-                                    let polygonCoordinates = geometry["coordinates"]
-                                    print(polygonCoordinates)
-                                
+                                    
+                                    let polygonCoordinate = geometry["coordinates"]
+                                    let polygonCoordinates = polygonCoordinate as! [[[Double]]]
+                                    
+                                    print(polygonCoordinates.count)
+                                    
+//                                    for polygonCordinate in polygonCoordinates {
+//                                    
+//                                    
+//                                    }
+                                    
                                 } else if geometry["type"] as! String == "MultiPolygon"{
-                                
-                                    let multiPolygonCoordinates = geometry["coordinates"]
-                                    print(multiPolygonCoordinates)
+                                    
+                                    let multiPolygonCoordinate = geometry["coordinates"]
+                                    let multiPolygonCoordinates = multiPolygonCoordinate as! [[[[Double]]]]
+                                    
+                                    print(multiPolygonCoordinates.count)
                                 } else {
-                                
-                                 print(geometry["type"] as! String)
+                                    
+                                    print(geometry["type"] as! String)
                                 }
                             }
                             
                             
-                            
-                           
-                        
                         }
                         
                     })
-                
+                    
                 }
             }
         }
@@ -139,5 +147,42 @@ class HazardsViewController: UIViewController,MKMapViewDelegate,CLLocationManage
         super.didReceiveMemoryWarning()
     }
     
+    
+    func drawShape() {
+        // Create a coordinates array to hold all of the coordinates for our shape.
+        var coordinate = [
+            CLLocationCoordinate2D(latitude: 45.522585, longitude: -122.685699),
+            CLLocationCoordinate2D(latitude: 45.534611, longitude: -122.708873),
+            CLLocationCoordinate2D(latitude: 45.530883, longitude: -122.678833),
+            CLLocationCoordinate2D(latitude: 45.547115, longitude: -122.667503),
+            CLLocationCoordinate2D(latitude: 45.530643, longitude: -122.660121),
+            CLLocationCoordinate2D(latitude: 45.533529, longitude: -122.636260),
+            CLLocationCoordinate2D(latitude: 45.521743, longitude: -122.659091),
+            CLLocationCoordinate2D(latitude: 45.510677, longitude: -122.648792),
+            CLLocationCoordinate2D(latitude: 45.515008, longitude: -122.664070),
+            CLLocationCoordinate2D(latitude: 45.502496, longitude: -122.669048),
+            CLLocationCoordinate2D(latitude: 45.515369, longitude: -122.678489),
+            CLLocationCoordinate2D(latitude: 45.506346, longitude: -122.702007),
+            CLLocationCoordinate2D(latitude: 45.522585, longitude: -122.685699),
+            ]
+        let polygon = MKPolygon(coordinates: &coordinate, count: 13)
+        self.mapView.addOverlay(polygon)
+        
+ //       var  p = [MKPolygon polygonWithPoints:coordinates count:4];
+//        CLLocationCoordinate2D coords[4] = {CLLocationCoordinate2DMake(40.5, -94.5),
+//            CLLocationCoordinate2DMake(40.6, -94.3),
+//            CLLocationCoordinate2DMake(40.5, -94.2),
+//            CLLocationCoordinate2DMake(40.5, -94.5)};
+           }
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        if overlay is MKPolygon {
+            let polygonView = MKPolygonRenderer(overlay: overlay)
+            polygonView.strokeColor = UIColor.magentaColor()
+            
+            return polygonView
+        }
+        
+        return nil
+    }
 }
 

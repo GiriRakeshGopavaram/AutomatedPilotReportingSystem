416aae23948e021535ae199803318ed552989b85
admin <admin@admins-MacBook-Pro.local>
2017-02-19 15:21:05 -0600

    Hazards are now color coded

--- AutomatedPIREP/HazardsViewController.swift
+++ AutomatedPIREP/HazardsViewController.swift
@@ -11,24 +11,139 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class HazardsViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelegate {
-    
+protocol HandleMapSearch {
+    func dropPinZoomIn(placemark:MKPlacemark)
+}
+class HazardsViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
+    var resultSearchController:UISearchController? = nil
+    var selectedPin:MKPlacemark? = nil
+    let locationManager = CLLocationManager()
     var pinAnnotationView:MKPinAnnotationView!
     var overlayToShow : MKOverlay!
-    
+    var colorToFill:String = ""
+    var startingLoc:CLLocationCoordinate2D!
+    var destinationLoc:CLLocationCoordinate2D!
+    var count = 0
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        let locationSearchTable:LocationSearchTable = storyboard!.instantiateViewControllerWithIdentifier("LocationSearchTable") as! LocationSearchTable
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable
+        
+        let searchBar = resultSearchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for places"
+        navigationItem.titleView = resultSearchController?.searchBar
+        
+        resultSearchController?.hidesNavigationBarDuringPresentation = false
+        resultSearchController?.dimsBackgroundDuringPresentation = true
+        definesPresentationContext = true
+        
+        locationSearchTable.mapView = mapView
+        locationSearchTable.handleMapSearchDelegate = self
+        mapView.delegate = self
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        
         mapView.delegate = self
         mapView.mapType = MKMapType.Standard
-        mapView.showsUserLocation = true
+        
+        locationSearchTable.handleMapSearchDelegate = self
+        definesPresentationContext = true
         makeRequest()
         
         
     }
     
+    //This function helps in drawing the lines across the mapview with respect to latitude and longitude.
+    func drawLines(latitude:Double, longitude:Double){
+        if mapView.annotations.count == 0 {
+            startingLoc = CLLocationCoordinate2D()
+            startingLoc.latitude = latitude
+            startingLoc.longitude = longitude
+        }
+        else if mapView.annotations.count == 1{
+            
+            destinationLoc = CLLocationCoordinate2D()
+            destinationLoc.latitude = latitude
+            destinationLoc.longitude = longitude
+        }
+        
+        if startingLoc != nil && destinationLoc != nil{
+
+            let sourceLocation:CLLocationCoordinate2D! = startingLoc
+            let destinationLocation:CLLocationCoordinate2D! = destinationLoc
+            // 3.
+            let sourcePlacemark = MKPlacemark(coordinate: sourceLocation, addressDictionary: nil)
+            let destinationPlacemark = MKPlacemark(coordinate: destinationLocation, addressDictionary: nil)
+            
+            // 4.
+            let sourceMapItem = MKMapItem(placemark: sourcePlacemark)
+            let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
+            
+            // 5.
+            let sourceAnnotation = MKPointAnnotation()
+            
+            if let location = sourcePlacemark.location {
+                sourceAnnotation.coordinate = location.coordinate
+            }
+            
+            
+            let destinationAnnotation = MKPointAnnotation()
+            
+            if let location = destinationPlacemark.location {
+                destinationAnnotation.coordinate = location.coordinate
+            }
+            
+            // 6.
+            self.mapView.showAnnotations([sourceAnnotation,destinationAnnotation], animated: true )
+            
+            // 7.
+            let directionRequest = MKDirectionsRequest()
+            directionRequest.source = sourceMapItem
+            directionRequest.destination = destinationMapItem
+            directionRequest.transportType = .Automobile
+            
+            // Calculate the direction
+            let directions = MKDirections(request: directionRequest)
+            
+            // 8.
+            directions.calculateDirectionsWithCompletionHandler {
+                (response, error) -> Void in
+                
+                guard let response = response else {
+                    if let error = error {
+                        print("Error: \(error)")
+                    }
+                    
+                    return
+                }
+                
+                let route = response.routes[0]
+                self.mapView.addOverlay((route.polyline), level: MKOverlayLevel.AboveRoads)
+                let rect = route.polyline.boundingMapRect
+                self.mapView.setRegion(MKCoordinateRegionForMapRect(rect), animated: true)
+            }
+            
+        }
+    }
+
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .AuthorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("error:: \(error)")
+    }
     func makeRequest(){
         let url = NSURL(string: "http://aviationweather.gov/gis/scripts/HazardJSON.php")
         // send out the request
@@ -54,7 +169,11 @@ class HazardsViewController: UIViewController,MKMapViewDelegate,CLLocationManage
                         
                         for eachObject in results {
                             let feature:[String:AnyObject] = eachObject["geometry"] as! [String:AnyObject]
-                            //let properties:[String:AnyObject] = eachObject["properties"] as! [String:AnyObject]
+                            let properties:[String:AnyObject] = eachObject["properties"] as! [String:AnyObject]
+                            if let color = properties["color"]{
+                                let colorAsString : String = "\(color)"
+                                self.colorToFill = colorAsString
+                            }
                             let type = feature["type"] as! NSString
                             let coordinates = feature["coordinates"] as! NSArray
                             var pointsToPlot:[CLLocationCoordinate2D] = []
@@ -64,8 +183,8 @@ class HazardsViewController: UIViewController,MKMapViewDelegate,CLLocationManage
                                     var count = 0
                                     if cast != nil{
                                         for location in  0 ..< coordinates[0][0].count{
-                                            let latitude = coordinates[0][0][location][0] as! Double
-                                            let longitude = coordinates[0][0][location][1] as! Double
+                                            let latitude = coordinates[0][0][location][1] as! Double
+                                            let longitude = coordinates[0][0][location][0] as! Double
                                             pointsToPlot.append(CLLocationCoordinate2DMake(latitude,longitude))
                                             count += 1
                                         }
@@ -74,7 +193,16 @@ class HazardsViewController: UIViewController,MKMapViewDelegate,CLLocationManage
                                         self.mapView.addOverlay(self.overlayToShow)
                                     }
                                     else{
-                                        //print(coordinates[0])
+                                        for location in  0 ..< coordinates[0].count{
+                                            let latitude = coordinates[0][location][1] as! Double
+                                            let longitude = coordinates[0][location][0] as! Double
+                                            pointsToPlot.append(CLLocationCoordinate2DMake(latitude,longitude))
+                                            count += 1
+                                        }
+                                        let mutablePointsToPlot: UnsafeMutablePointer<CLLocationCoordinate2D> = UnsafeMutablePointer(pointsToPlot)
+                                        self.overlayToShow = MKPolygon(coordinates: mutablePointsToPlot, count: count)
+                                        self.mapView.addOverlay(self.overlayToShow)
+
                                     }
                                 }
                                 else{
@@ -96,7 +224,6 @@ class HazardsViewController: UIViewController,MKMapViewDelegate,CLLocationManage
         }
         
     }
-
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -105,12 +232,102 @@ class HazardsViewController: UIViewController,MKMapViewDelegate,CLLocationManage
     func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
         if overlay is MKPolygon {
             let polygonView = MKPolygonRenderer(overlay: overlay)
-            polygonView.strokeColor = UIColor.magentaColor()
+            polygonView.fillColor = hexStringToUIColor(self.colorToFill)
+
             return polygonView
+        }
+        else{
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            renderer.strokeColor = UIColor.redColor()
+            renderer.lineWidth = 4.0
+            
+            return renderer
         }
         
         return nil
     }
-  
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet() as NSCharacterSet).uppercaseString
+        
+        if (cString.hasPrefix("#")) {
+            cString = cString.substringFromIndex(cString.startIndex.advancedBy(1))
+        }
+        
+        if ((cString.characters.count) != 6) {
+            return UIColor.grayColor()
+        }
+        
+        var rgbValue:UInt32 = 0
+        NSScanner(string: cString).scanHexInt(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
 }
+
+
+
+extension HazardsViewController: HandleMapSearch {
+    func dropPinZoomIn(placemark:MKPlacemark){
+        
+        // cache the pin
+        selectedPin = placemark
+        // clear existing pins
+        let annotation = MKPointAnnotation()
+        
+        if mapView.annotations.count == 1{
+            annotation.coordinate = placemark.coordinate
+            annotation.title = placemark.name
+            if let city = placemark.locality,
+                let state = placemark.administrativeArea {
+                annotation.subtitle = "\(city) \(state) -- Destination"
+                drawLines(annotation.coordinate.latitude, longitude: annotation.coordinate.longitude)
+                
+            }
+            mapView.addAnnotation(annotation)
+            
+            let span = MKCoordinateSpanMake(0.05, 0.05)
+            let region = MKCoordinateRegionMake(placemark.coordinate, span)
+            mapView.setRegion(region, animated: true)
+            
+            let alert:UIAlertController = UIAlertController(title: "Do you want to use this as your destination location?", message: "" , preferredStyle: .Alert)
+            let defaultAction:UIAlertAction =  UIAlertAction(title: "Yes", style: .Default, handler: nil)
+            alert.addAction(defaultAction)
+            let noAction:UIAlertAction = UIAlertAction(title:"No", style: .Cancel, handler: nil)
+            alert.addAction(noAction)
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+        }
+        else if mapView.annotations.count == 0{
+            
+            mapView.removeAnnotations(mapView.annotations)
+            annotation.coordinate = placemark.coordinate
+            annotation.title = placemark.name
+            if let city = placemark.locality,
+                let state = placemark.administrativeArea {
+                annotation.subtitle = "\(city) \(state) -- Starting Location"
+                drawLines(annotation.coordinate.latitude, longitude: annotation.coordinate.longitude)
+                
+            }
+            mapView.addAnnotation(annotation)
+            
+            let span = MKCoordinateSpanMake(0.05, 0.05)
+            let region = MKCoordinateRegionMake(placemark.coordinate, span)
+            mapView.setRegion(region, animated: true)
+            
+            let alert:UIAlertController = UIAlertController(title: "Do you want to use this as your current location?", message: "" , preferredStyle: .Alert)
+            let defaultAction:UIAlertAction =  UIAlertAction(title: "Yes", style: .Default, handler: nil)
+            alert.addAction(defaultAction)
+            let noAction:UIAlertAction = UIAlertAction(title:"No", style: .Cancel, handler: nil)
+            alert.addAction(noAction)
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+        }
+    }
+}
+
 
---
+++ AutomatedPIREP/LocationSearchTable.swift
@@ -0,0 +1,101 @@
+//
+//  LocationSearchTable.swift
+//  AutomatedPIREP
+//
+//  Created by admin on 2/12/17.
+//  Copyright © 2017 Gopavaram,Giri Rakesh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class LocationSearchTable: UITableViewController {
+    var selectedPin:MKPlacemark? = nil
+    var resultSearchController:UISearchController? = nil
+    var matchingItems:[MKMapItem] = []
+    var mapView: MKMapView? = nil
+    var handleMapSearchDelegate:HandleMapSearch? = nil
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let locationSearchTable = storyboard!.instantiateViewControllerWithIdentifier("LocationSearchTable") as! LocationSearchTable
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable
+        locationSearchTable.mapView = mapView
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    //It helps in processing the address of the current user and also takes the input of current address of the user automatically by allowing the application to access the location. It can also take the address by allowing the user to mark the destination and starting points.
+    func parseAddress(selectedItem:MKPlacemark) -> String {
+        // put a space between "4" and "Melrose Place"
+        let firstSpace = (selectedItem.subThoroughfare != nil && selectedItem.thoroughfare != nil) ? " " : ""
+        // put a comma between street and city/state
+        let comma = (selectedItem.subThoroughfare != nil || selectedItem.thoroughfare != nil) && (selectedItem.subAdministrativeArea != nil || selectedItem.administrativeArea != nil) ? ", " : ""
+        // put a space between "Washington" and "DC"
+        let secondSpace = (selectedItem.subAdministrativeArea != nil && selectedItem.administrativeArea != nil) ? " " : ""
+        let addressLine = String(
+            format:"%@%@%@%@%@%@%@",
+            // street number
+            selectedItem.subThoroughfare ?? "",
+            firstSpace,
+            // street name
+            selectedItem.thoroughfare ?? "",
+            comma,
+            // city
+            selectedItem.locality ?? "",
+            secondSpace,
+            // state
+            selectedItem.administrativeArea ?? ""
+        )
+        return addressLine
+    }
+    
+}
+
+
+//It provides search table for searching the location on the map.
+extension LocationSearchTable : UISearchResultsUpdating {
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        guard let mapView = mapView,
+            let searchBarText = searchController.searchBar.text else { return }
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = searchBarText
+        request.region = mapView.region
+        let search = MKLocalSearch(request: request)
+        search.startWithCompletionHandler { response, _ in
+            guard let response = response else {
+                return
+            }
+            self.matchingItems = response.mapItems
+            self.tableView.reloadData()
+        }
+    }
+}
+// It is a function which provides a tableview for the location search and returns the cell which contains the location information
+extension LocationSearchTable {
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return matchingItems.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell")!
+        let selectedItem = matchingItems[indexPath.row].placemark
+        cell.textLabel?.text = selectedItem.name
+        cell.detailTextLabel?.text = parseAddress(selectedItem)
+        return cell
+    }
+}
+
+
+// It is a function which helps in obtaining the location on a table view search by selecting on a particular row of the table view
+extension LocationSearchTable {
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let selectedItem = matchingItems[indexPath.row].placemark
+        handleMapSearchDelegate?.dropPinZoomIn(selectedItem)
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
